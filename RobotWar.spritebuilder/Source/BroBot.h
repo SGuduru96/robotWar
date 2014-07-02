bae313495cd4eae75969fba5078430241a68d457
Shashank Guduru <shashank.guduru@gmail.com>
2014-07-02 16:39:20 -0700

    This is the basic code for brobot

---
+++ RobotWar.spritebuilder/Source/BroBot.h
@@ -0,0 +1,13 @@
+//
+//  BroBot.h
+//  RobotWar
+//
+//  Created by Shashank Guduru on 7/2/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Robot.h"
+
+@interface BroBot : Robot
+
+@end
